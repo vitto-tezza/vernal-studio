4b37dcf438a0b715d27b0cc5b0af5d638644db3c
Diego Matera <diegomatera@gmail.com>
2022-07-31 19:31:12 -0300

    Se terminó estructura de secciones footer y bannerup versión mobile

--- src/components/BannerUp/BannerUp.js
+++ src/components/BannerUp/BannerUp.js
@@ -5,15 +5,34 @@ import BannerUpDot from "../../utils/images/bannerup-dot.svg";
 const BannerUp = () => {
   return (
     <div className="BannerUp">
-      <div className="BannerUp-Slider">
-        <img
-          src={BannerUpDot}
-          alt="slider-image"
-          className="BannerUp-Slider-Image"
-        ></img>
-        <p className="BannerUp-Slider-Text">Evolucionemos juntos</p>
+      <div className="BannerUp-Slider-Container">
+        <div className="BannerUp-Slider">
+          <img
+            src={BannerUpDot}
+            alt="slider-image"
+            className="BannerUp-Slider-Image"
+          ></img>
+          <p className="BannerUp-Slider-Text">Evolucionemos juntos</p>
+          <img
+            src={BannerUpDot}
+            alt="slider-image"
+            className="BannerUp-Slider-Image"
+          ></img>
+          <p className="BannerUp-Slider-Text">Evolucionemos juntos</p>
+          <img
+            src={BannerUpDot}
+            alt="slider-image"
+            className="BannerUp-Slider-Image"
+          ></img>
+          <p className="BannerUp-Slider-Text">Evolucionemos juntos</p>
+          <img
+            src={BannerUpDot}
+            alt="slider-image"
+            className="BannerUp-Slider-Image"
+          ></img>
+          <p className="BannerUp-Slider-Text">Evolucionemos juntos</p>
+        </div>
       </div>
-
       <div className="BannerUp-Button-Container">
         <a href="www.vernalstudio.com" className="BannerUp-Button">
           <figure className="BannerUp-Button-Image"></figure>
