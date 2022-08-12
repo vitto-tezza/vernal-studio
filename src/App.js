5c03b40994705eed56f0d6d98f3e752be298c47e
vitto tezza <vittorio.tezza93@gmail.com>
2022-08-12 15:33:22 -0300

    prueba deploy 3

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
+        <HomePage />
         <BannerUp />
         <Footer />
       </BrowserRouter>
