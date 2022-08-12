417a99ee0def7a422079bf7f3a2b670e3fe7dfaa
vitto tezza <vittorio.tezza93@gmail.com>
2022-08-12 15:13:35 -0300

    Updates

---
+++ precache-manifest.dd3ee36df5a69b655c3af2ee53df981e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "7378431da60d90697afa",
+    "url": "/vernal-studio/static/css/main.7c8df249.chunk.css"
+  },
+  {
+    "revision": "7378431da60d90697afa",
+    "url": "/vernal-studio/static/js/main.7378431d.chunk.js"
+  },
+  {
+    "revision": "9f6f6cec55adb720d38c",
+    "url": "/vernal-studio/static/js/1.9f6f6cec.chunk.js"
+  },
+  {
+    "revision": "6cc5d634ab71c74b2a62",
+    "url": "/vernal-studio/static/js/2.6cc5d634.chunk.js"
+  },
+  {
+    "revision": "22c9318bf6126a085182",
+    "url": "/vernal-studio/static/js/runtime~main.22c9318b.js"
+  },
+  {
+    "revision": "b1224daac2af8f432fb940eadb215cfa",
+    "url": "/vernal-studio/static/media/bannerup-dot.b1224daa.svg"
+  },
+  {
+    "revision": "d1273838a4ce1f708e1b5194964f86df",
+    "url": "/vernal-studio/static/media/foto-hero.d1273838.png"
+  },
+  {
+    "revision": "54013e870807114f18c59739ea2e71c7",
+    "url": "/vernal-studio/static/media/hero-descriptionarrow.54013e87.svg"
+  },
+  {
+    "revision": "727620522ee7d026d4c1d6c284dd155f",
+    "url": "/vernal-studio/static/media/projects-titlearrow.72762052.svg"
+  },
+  {
+    "revision": "758f5912aad8934e753acba03851fbdf",
+    "url": "/vernal-studio/static/media/projects-loop3-logo.758f5912.png"
+  },
+  {
+    "revision": "88d3aab40d4b46b15e78b14c80d822ca",
+    "url": "/vernal-studio/static/media/projects-maderotango-logo.88d3aab4.png"
+  },
+  {
+    "revision": "b153feb140469fe89304b53ca682347e",
+    "url": "/vernal-studio/static/media/projects-daleplast-logo.b153feb1.png"
+  },
+  {
+    "revision": "1be1bbbfc3f050309978b27efcdf987c",
+    "url": "/vernal-studio/static/media/projects-tuespacio-logo.1be1bbbf.svg"
+  },
+  {
+    "revision": "7845ee289c2864c2812ac188d5e953e1",
+    "url": "/vernal-studio/static/media/creativeprocess-bg.7845ee28.png"
+  },
+  {
+    "revision": "dc5cdac8ee2297801c284912e5773e10",
+    "url": "/vernal-studio/static/media/footer-logo.dc5cdac8.svg"
+  },
+  {
+    "revision": "b0e07b847464f2cdf00d1735a6da0f78",
+    "url": "/vernal-studio/static/media/projects-boxarrow.b0e07b84.svg"
+  },
+  {
+    "revision": "e6ea4336442d4262a10c7c4fdf89af7a",
+    "url": "/vernal-studio/static/media/projects-morearrow.e6ea4336.svg"
+  },
+  {
+    "revision": "c83413874c8deed7742d87c3d4d7ca05",
+    "url": "/vernal-studio/static/media/projects-maderotango-bg.c8341387.png"
+  },
+  {
+    "revision": "25c5ec62af790df448ab2b650ec646bc",
+    "url": "/vernal-studio/static/media/projects-tuespacio-bg.25c5ec62.png"
+  },
+  {
+    "revision": "e6dc731ab6243d6d714510e62cf07f3e",
+    "url": "/vernal-studio/static/media/projects-loop3-bg.e6dc731a.png"
+  },
+  {
+    "revision": "a6f05b9f8b0d6b2fc7985a03b8ba13c0",
+    "url": "/vernal-studio/static/media/projects-daleplast-bg.a6f05b9f.png"
+  },
+  {
+    "revision": "19094b5dc2ce64f59d013bd060fa0edb",
+    "url": "/vernal-studio/static/media/aboutus-web-design.19094b5d.png"
+  },
+  {
+    "revision": "4e9be66807a3ef38755491bcc94dcee6",
+    "url": "/vernal-studio/static/media/creativeprocess-icon1.4e9be668.svg"
+  },
+  {
+    "revision": "4898b7a73a588dfc513b415c174f47a3",
+    "url": "/vernal-studio/static/media/creativeprocess-icon2.4898b7a7.svg"
+  },
+  {
+    "revision": "f40f7b067aeedd8212cf6e3d58e8e7fb",
+    "url": "/vernal-studio/static/media/creativeprocess-icon3.f40f7b06.svg"
+  },
+  {
+    "revision": "375bd77c43baddbb70106e83f400cc4f",
+    "url": "/vernal-studio/static/media/creativeprocess-icon4.375bd77c.svg"
+  },
+  {
+    "revision": "598f47824f6866ee488bdf100c5e4240",
+    "url": "/vernal-studio/static/media/chat-wsp.598f4782.svg"
+  },
+  {
+    "revision": "2e085238b20061f59cbb5b888a047be5",
+    "url": "/vernal-studio/static/media/bannerup-arrow.2e085238.svg"
+  },
+  {
+    "revision": "fbacd13a977a49be59e990f673834fad",
+    "url": "/vernal-studio/static/media/footer-ig.fbacd13a.svg"
+  },
+  {
+    "revision": "dce73346b769bf5acd6d5fe9a04550f4",
+    "url": "/vernal-studio/static/media/footer-in.dce73346.svg"
+  },
+  {
+    "revision": "0609d1fa49b0c3336bbca550c45b17e2",
+    "url": "/vernal-studio/static/media/footer-phone.0609d1fa.svg"
+  },
+  {
+    "revision": "ff75875b19d43050b7edce29109a0ac0",
+    "url": "/vernal-studio/static/media/footer-behance.ff75875b.svg"
+  },
+  {
+    "revision": "216cbb28b740790ad1c4b66a55f0bcc1",
+    "url": "/vernal-studio/index.html"
+  }
+];
